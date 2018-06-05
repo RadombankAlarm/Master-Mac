8dfc19f6d128e1e6d72137e9c6bd02134246bdcb
RadombankAlarm <39938902+RadombankAlarm@users.noreply.github.com>
2018-06-05 14:39:09 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -14,8 +14,24 @@ if (!is_null($events['events'])) {
 			$touserid = $event['source']['userId'];
 			$toroomid = $event['source']['roomId'];
 			$togroupid = $event['source']['groupId'];
+						
+			// Find User Data
+			$url = 'https://api.line.me/v2/bot/profile/'.$touserid;
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			$result = curl_exec($ch);
+			curl_close($ch);
+			//echo $result . "\r\n";
+			$events = json_decode($result, true);
+			// Make Push Messageing
+			$displayName = $events['displayName'];
+			$userId = $events['userId'];			
+			
 			// Gen Text Reply
-			$gentext = "ขอบคุณที่ติดตามเรา";
+			$gentext = "ขอบคุณที่ติดตามเรา"." \n".$displayName." User\n".$userId;
 			// Get Replytoken
 			$replyToken = $event['replyToken'];
 			//Make a POST Request to Messaging API to reply to follower
@@ -37,73 +53,8 @@ if (!is_null($events['events'])) {
 
 			echo $result . "\r\n";
 			
-			// Find User Data
-			$url = 'https://api.line.me/v2/bot/profile/'.$touserid;
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "GET");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			$result = curl_exec($ch);
-			curl_close($ch);
-			//echo $result . "\r\n";
-			$events = json_decode($result, true);
-			// Make Push Messageing
-			$displayName = $events['displayName'];
-			$userId = $events['userId'];
-			$text = $displayName." User\n".$userId;
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-				//.'\nRequest '.$reqtext
-			];
-			$url = 'https://api.line.me/v2/bot/message/push';
-			$data = [
-				'to' => 'U554a18dbd36996fdb3dd95c218cf6db0',
-				'messages' => [$messages]
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-			
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
+		
 
-			echo $result . "\r\n";
-			
-			// To group Mac Share
-
-			
-			// Find Group Data
-			$text = "Group\n".$togroupid;
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-				//.'\nRequest '.$reqtext
-			];
-			$url = 'https://api.line.me/v2/bot/message/push';
-			$data = [
-				'to' => 'U554a18dbd36996fdb3dd95c218cf6db0',
-				'messages' => [$messages]
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-			
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
 		}    
   }
 }
