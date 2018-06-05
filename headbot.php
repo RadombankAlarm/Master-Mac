fb2a26a24f27e9f3135ab82f05021712d5017dae
RadombankAlarm <39938902+RadombankAlarm@users.noreply.github.com>
2018-06-05 13:54:53 +0700

    Update headbot.php

--- headbot.php
+++ headbot.php
@@ -2,7 +2,5 @@
 
 $access_token = 'XAnBaVQQ/eaDIJDjK0uOuTxNlxwH7ZZmzOraW/E1f9fsr13sNAu6gMu/bDtMGKRs1CJnHNbtlmv6i23Jp2xmntQOADRqKegBT5+naaey9vig+EAdWXwoWMOYHlKn6TwmIpc7Y9Wj6zvbCn9Vmgu1VwdB04t89/1O/w1cDnyilFU=';
 
-//https://still-inlet-83395.herokuapp.com/
-
 
 ?>
